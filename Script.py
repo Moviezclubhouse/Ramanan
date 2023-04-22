924fb519dfaa41709a151ad9951565130e66b068
Premimum <129716070+Premimum@users.noreply.github.com>
2023-04-22 09:08:12 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -209,7 +209,7 @@ IMDb Data:
 📆 Year: <a href={url}/releaseinfo>{year}</a>
 🌟 Rating: <a href={url}/ratings>{rating}</a> / 10 (based on {votes} user ratings.)
 ☀️ Languages : <code>{languages}</code>
-🔖 Story Line :{plot}
+🔖 Story Line :<code>{plot}</code>
 
 🕵️ Requested by : {message.from_user.mention}</b>"""
 
