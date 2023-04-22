f07c62a7d4f6e3883e7da157f370195223b4defb
Premimum <129716070+Premimum@users.noreply.github.com>
2023-04-22 22:25:41 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1222,7 +1222,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
     elif query.data == "owner_info":
             btn = [[
                     InlineKeyboardButton("⟸ Bᴀᴄᴋ", callback_data="start"),
-                    InlineKeyboardButton("Cᴏɴᴛᴀᴄᴛ", url="http://t.me/MMAssistBot")
+                    InlineKeyboardButton("Cᴏɴᴛᴀᴄᴛ", url="http://t.me/CK_HELPER")
                   ]]
             await client.edit_message_media(
                 query.message.chat.id, 
